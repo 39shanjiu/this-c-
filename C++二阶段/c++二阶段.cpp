de8db86238fa70363873c12bcd9b606496ccc525
39shanjiu <1918251321@qq.com>
2022-05-07 22:16:08 +0800

    c++刷题要步入正规了

--- C++二阶段/c++二阶段.cpp
+++ C++二阶段/c++二阶段.cpp
@@ -228,33 +228,205 @@ using namespace std;
 //	p.showperson();
 //	return 0;
 //}
-template<class T1, class T2>
-class Person;
-
-template<class T1, class T2>
-void showperson(Person<T1, T2>& p)
+//template<class T1, class T2>
+//class Person;
+//
+//template<class T1, class T2>
+//void showperson(Person<T1, T2>& p)
+//{
+//	cout << p.m_Name << "  " << p.m_Age << endl;
+//}
+//
+//template<class T1,class T2>
+//class Person
+//{
+//	friend void showperson<>(Person<T1,T2>&p);
+//public:
+//	Person(T1 name, T2 age)
+//	{
+//		this->m_Name = name;
+//		this->m_Age = age;
+//	}
+//private:
+//	T1 m_Name;
+//	T2 m_Age;
+//};
+//
+//int main()
+//{
+//	Person<string, int>p("Jerry", 18);
+//	showperson(p);
+//	return 0;
+//}
+/*
+#include"Ä£°å.hpp"
+template<class T>
+class MyArray
 {
-	cout << p.m_Name << "  " << p.m_Age << endl;
-}
-
-template<class T1,class T2>
-class Person
-{
-	friend void showperson<>(Person<T1,T2>&p);
 public:
-	Person(T1 name, T2 age)
+	MyArray(int capacity)
 	{
-		this->m_Name = name;
-		this->m_Age = age;
+		this->m_Capacity = capacity;
+		this->m_Size = 0;
+		this->m_Arr = new T[this->m_Capacity];
 	}
+
+	MyArray(MyArray<T>& p)
+	{
+		this->m_Capacity = p.m_Capacity;
+		this->m_Size = p.m_Size;
+		this->m_Arr = new T[p.m_Capacity];
+		for (int i = 0; i < this->m_Size; i++)
+		{
+			this->m_Arr[i] = p.m_Arr[i];
+		}
+
+	}
+	MyArray& operator=(MyArray<T>& p)
+	{
+		if (!this->m_Arr)
+		{
+			delete[] this->m_Arr;
+			this->m_Arr = NULL;
+		}
+		this->m_Capacity = p.m_Capacity;
+		this->m_Arr = new T[this->m_Capacity];
+		this->m_Size = p.m_Size;
+		for (int i = 0; i < p.m_Size; i++)
+		{
+			this->m_Arr[i] = p.m_Arr[i];
+		}
+		return *this;
+	}
+
+	~MyArray()
+	{
+		if (!this->m_Arr)
+		{
+			delete[] this->m_Arr;
+			this->m_Arr = NULL;
+		}
+
+	}
+	void Add_Tail(T num)
+	{
+		this->m_Arr[this->m_Size] = num;
+		this->m_Size++;
+	}
+
+	void Del_Tail()
+	{
+		if (this->m_Size == 0)
+			return;
+		this->m_Arr[this->m_Size - 1] = 0;
+		this->m_Size--;
+	}
+
+	void showArr()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			cout << this->m_Arr[i] << " ";
+		}
+		cout << endl;
+	}
+
 private:
-	T1 m_Name;
-	T2 m_Age;
+	T* m_Arr;
+	int  m_Capacity;
+	int m_Size;
 };
 
 int main()
 {
-	Person<string, int>p("Jerry", 18);
-	showperson(p);
+	MyArray<int>arr1(10);
+	arr1.Add_Tail(10);
+	arr1.Add_Tail(20);
+	MyArray<int>arr2 = arr1;
+	arr2.Add_Tail(30);
+	arr2.Add_Tail(50);
+	MyArray<char>s(5);
+	s.Add_Tail('6');
+	s.Add_Tail('7');
+	s.Add_Tail('8');
+//	s.Add_Tail('9');
+//	s.Add_Tail('0');
+//	arr2.Del_Tail();
+//	s.Del_Tail();
+//	s.showArr();
+//	arr1.showArr();
+//	arr2.showArr();
+//	MyArray<int>arr3(10);
+//	arr3 = arr2;
+//	arr3.showArr();
+//	return 0;
+//}*/
+
+#include <vector>
+
+//int main()
+//{
+//	vector<vector<int>>v;
+//	vector<int>v1;
+//	vector<int>v2;
+//	vector<int>v3;
+//	vector<int>v4;
+//	for (int j = 0; j < 4; j++)
+//	{
+//		v1.push_back(j + 1);
+//		v2.push_back(j + 2);
+//		v3.push_back(j + 3);
+//		v4.push_back(j + 4);
+//	}
+//	v.push_back(v1);
+//	v.push_back(v2);
+//	v.push_back(v3);
+//	v.push_back(v4);
+//	for (int i = 0; i < v.size(); i++)
+//	{
+//		for (int j = 0; j < v[i].size(); j++)
+//			cout << v[i][j] << "  ";
+//		cout << endl;
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	//int n = 5;
+//	//string s(n, 97);
+//	//cout << s[4] << endl;
+//
+//	/*string s = "hello";
+//	s += " ";
+//	s += "wrold";
+//	cout << s << endl;
+//	string p = "nihao shijie";
+//	s.append(p,6,6);
+//	cout << s << endl;*/
+//	//string s = "jkafde@fghijk";
+//	//cout << s.find("cdd") << endl;
+//	/*cout << s.rfind("jk") << endl;
+//	cout << s.find("jk") << endl;*/
+//	//s.replace(1, 0, "1111");
+//	//cout << s << endl;
+//	//cout << s.capacity() << endl;
+//	//cout << s.size() << endl;
+//	//cout << s.compare("jkA");
+//	//s.insert(1, "123");
+//	//s.erase(1, 3);
+//	//string substr = s.substr(1, 8);
+//
+//	/*cout << substr << endl;
+//	
+//	return 0;*/
+//}
+#include <string.h>
+#include <algorithm>
+
+int main()
+{
+	string s = "hello wrold";
+	cout << s.begin() << endl;
 	return 0;
 }
