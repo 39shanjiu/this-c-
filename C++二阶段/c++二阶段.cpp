d2af503c6154aac3c7d1b5f6e4b98de776b47093
39shanjiu <1918251321@qq.com>
2022-05-12 21:18:50 +0800

    加油

--- C++二阶段/c++二阶段.cpp
+++ C++二阶段/c++二阶段.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-using namespace std;
+
 
 //template<typename T>
 //void My_Swap(T& a, T& b)
@@ -421,6 +420,260 @@ int main()
 //	
 //	return 0;*/
 //}
+
+////int main()
+////{
+////	/*string s = "hello wrold";
+////	cout << s.begin() << endl;*/
+////	vector<int>v;
+////	for (int i = 0; i < 6; i++)
+////		v.push_back(i + 1);
+////	del(v, sizeof(int), i);
+////	return 0; 
+////}
+//
+//class Player
+//{
+//public:
+//	Player(string name, int score)
+//	{
+//		this->m_Name = name;
+//		this->m_Score = score;
+//	}
+//
+//	string m_Name;
+//
+//	int m_Score;
+//};
+//
+//void creatPlayer(vector<Player>& v)
+//{
+//	for (int i = 0; i < 5; i++)
+//	{
+//		Player p("选手", 0);
+//		p.m_Name += ('A' + i);
+//		v.push_back(p);
+//	}
+//}
+//
+//void setScore(vector<Player>& v)
+//{
+//	for (int i = 0; i < v.size(); i++)
+//	{
+//		deque<int>queue;
+//		for (int j = 0; j < 10; j++)
+//		queue.push_back(rand() % 41 + 60);
+//		sort(queue.begin(), queue.end());
+//		queue.pop_back();
+//		queue.pop_front();
+//		int sum = 0;
+//		for (int j = 0; j < queue.size(); j++)
+//		sum += queue[i];
+//		v[i].m_Score = sum / (int)queue.size();
+//	}
+//}
+//
+//void PlantPlayer(vector<Player>& v)
+//{
+//	for (int i = 0; i < v.size(); i++)
+//		cout << "name:  " << v[i].m_Name << "\tScore:  " << v[i].m_Score << endl;
+//}
+//
+//
+//void test1()
+//{
+//	vector<Player>v;
+//	creatPlayer(v);
+//
+//	setScore(v);
+//
+//	PlantPlayer(v);
+//}
+//
+//void List()
+//{
+//	list<int>L1;
+//	for (int i = 0; i < 10; i++)
+//		L1.push_back(10 * i);
+//	L1.insert(++L1.begin(),1000);
+//	list<int>L2(L1.begin(),L1.end() );
+//}
+//
+////class Person
+////{
+////public:
+////	string m_Name;
+////	int m_Age;
+////};
+////
+////void test2()
+////{
+////	set<Person>s;
+////
+////}
+//
+//void test3()
+//{
+//	map<int, int>m;
+//	m.insert(pair<int, int>(1, 10));
+//	m.insert(pair<int, int>(5, 50));
+//	m.insert(pair<int, int>(3, 30));
+//	m.insert(pair<int, int>(4, 40));
+//	for (int i = 1; i <= m.size(); ++i)
+//		cout << m[i] << "  ";
+//	for (map<int,int>::iterator it = m.begin(); it != m.end(); it++)
+//	{
+//		cout << it->first << "  " << it->second << "  " << endl;
+//	}
+//	map<int, int>m1(m);
+//	for (map<int, int>::iterator it = m1.begin(); it != m1.end(); it++)
+//	{
+//		cout << it->first << "  " << it->second << "  " << endl;
+//	}
+//	map<int, int>m2 = m1;
+//	for (map<int, int>::iterator it = m2.begin(); it != m2.end(); it++)
+//	{
+//		cout << it->first << "  " << it->second << "  " << endl;
+//	}
+//
+//}
+//
+//class Emplayee
+//{
+//public:
+//	Emplayee(string name, int salary)
+//	{
+//		this->m_Name = name;
+//		this->m_Salary = salary;
+//	}
+//
+//	
+//
+//	string m_Name;
+//
+//	int m_Salary;
+//
+//	string m_Branch;
+//
+//	int m_Size;
+//};
+//
+//void operator<<(ostream& cout,Emplayee& p)
+//{
+//	cout << "姓名: " << p.m_Name << "\t所属部门: " << p.m_Branch << "\t薪资: " << p.m_Salary << endl;;
+//}
+//
+//void CreatEmplayee(vector<Emplayee>& v)
+//{
+//	for (int i = 0; i < 10; ++i)
+//	{
+//		string s = "ABCDEFGHIJ";
+//		string name = "员工";
+//		name += s[i];
+//		Emplayee p(name, 0);
+//		v.push_back(p);
+//	}
+//}
+//
+//void DisEmp(vector<Emplayee>&v,multimap<int,Emplayee>&m)
+//{
+//	for (int i = 0; i < v.size(); ++i)
+//	{
+//		v[i].m_Size = rand() % 3 + 1;
+//		switch (v[i].m_Size)
+//		{
+//		case 2:
+//			v[i].m_Branch = "研发";
+//			v[i].m_Salary = rand() % 5000 + 8000;
+//			break;
+//		case 3:
+//			v[i].m_Branch = "策划";
+//			v[i].m_Salary = rand() % 5000 + 5000;
+//			break;
+//		case 1:
+//			v[i].m_Branch = "美术";
+//			v[i].m_Salary = rand() % 5000 + 11000;
+//			break;
+//		default:
+//			break;
+//		}
+//		m.insert(pair<int,Emplayee>(v[i].m_Size,v[i]));
+//	}
+//}
+//
+//void PrintEmp(multimap<int, Emplayee>& m)
+//{
+//	for (multimap<int, Emplayee>::iterator it = m.begin(); it != m.end(); ++it)
+//	{
+//		cout << it->second;
+//	}
+//}
+//
+//void test4()
+//{
+//	vector<Emplayee>v;
+//	multimap<int, Emplayee>m;
+//	CreatEmplayee(v);
+//	DisEmp(v, m);
+//	PrintEmp(m);
+//	//v[0].m_Size = 1;
+//	//v[0].m_Salary = 3000;
+//	//v[0].m_Branch = "美术";
+//	//cout << v[0];
+//}
+//
+//void Build_Tree(int arr[], int tree[], int end, int start, int node)
+//{
+//	if (end == start)
+//	tree[node] = arr[end];
+//	else {
+//		int mid = (start + end) / 2;
+//		int node_left  = node * 2 + 1;
+//		int node_right = node * 2 + 2;
+//
+//		Build_Tree(arr, tree, mid, start, node_left);
+//		Build_Tree(arr, tree, end, mid + 1, node_right);
+//		tree[node] = tree[node_left] + tree[node_right];
+//	}
+//}
+//
+//void updata_tree(int arr[], int tree[], int end, int start, int node, int idx, int val) {
+//	if (end == start && end == idx)
+//	{
+//		arr[idx] = val;
+//		tree[node] = arr[idx];
+//	}
+//	else {
+//		int mid = (start + end) / 2;
+//		if (idx > mid)
+//		updata_tree(arr, tree, end, mid + 1, 2 * node + 2, idx, val);
+//		else 
+//		updata_tree(arr, tree, mid,start, 2 * node + 1, idx, val);
+//		tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+//	}
+//}
+//
+//void test()
+//{
+//	int arr[] = { 1,3,5,7,9,11 };
+//	int size = 6;
+//	int tree[15] = { 0 };
+//
+//	Build_Tree(arr, tree, size - 1, 0, 0);
+//
+//	updata_tree(arr, tree, size - 1, 0, 0, 4, 6);
+//	for (int i = 0; i < 15; i++)
+//	{
+//		cout << tree[i] << " ";
+//	}
+//}
+//
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
 #include <string.h>
 #include <algorithm>
 #include <vector>
@@ -428,251 +681,118 @@ int main()
 #include <list>
 #include <set>
 #include <map>
-//int main()
+#include <iostream>
+#include <functional>
+using namespace std;
+
+//int main1()
 //{
-//	/*string s = "hello wrold";
-//	cout << s.begin() << endl;*/
-//	vector<int>v;
-//	for (int i = 0; i < 6; i++)
-//		v.push_back(i + 1);
-//	del(v, sizeof(int), i);
-//	return 0; 
+//	vector<int>nums = { 8,7,1,10,17,15,18,11,16,9,19,12,5,14,3,4,2,13,18,18 };
+//	int sz = nums.size();
+//	for (int i = 0; i < sz; ++i)
+//	{
+//		if (i + 1 != nums[i] && nums[nums[i] - 1] == nums[i])
+//			cout << "Yes";
+//		if (i + 1 != nums[i])
+//		{
+//			swap(nums[nums[i] - 1], nums[i]);
+//			--i;
+//		}
+//	}
+//	
+//
+//	return 0;
+//}
+//
+//int main2()
+//{
+//	vector<int>nums = { 7,6,5,4,3,2,1,0,-1,-2 };
+//	int k = 3;
+//	int sz = nums.size();
+//	//if (k == 1)
+//		//return nums;
+//	int r = -10086;
+//	int l = -10086;
+//	int i = 0;
+//	int posr = 0;
+//	int posl = 0;
+//	for (i = 0; i < k; ++i)
+//	{
+//		if (nums[i] >= r)
+//		{
+//			if (nums[i] >= l)
+//			{
+//				l = nums[i];
+//				posl = i;
+//				r = -10086;
+//			}
+//			else
+//			{
+//				r = nums[i];
+//				posr = i;
+//			}
+//		}
+//	}
+//	vector<int>ans;
+//	ans.push_back(l);
+//	for (; i < nums.size(); ++i)
+//	{
+//		if (nums[i] >= r)
+//		{
+//			if (nums[i] >= l)
+//			{
+//				l = nums[i];
+//				posl = i;
+//				r = -10086;
+//			}
+//			else
+//			{
+//				r = nums[i];
+//				posr = i;
+//			}
+//		}
+//		if (posl == i - k)
+//		{
+//			l = r;
+//			posl = posr;
+//			r = -10086;
+//		}
+//		ans.push_back(l);
+//	}
+//	//return ans;
+//	return 0;
 //}
 
-class Player
+class five
 {
 public:
-	Player(string name, int score)
+	bool operator() (int val)
 	{
-		this->m_Name = name;
-		this->m_Score = score;
+		return val > 5;
 	}
-
-	string m_Name;
-
-	int m_Score;
 };
-
-void creatPlayer(vector<Player>& v)
-{
-	for (int i = 0; i < 5; i++)
-	{
-		Player p("选手", 0);
-		p.m_Name += ('A' + i);
-		v.push_back(p);
-	}
-}
-
-void setScore(vector<Player>& v)
-{
-	for (int i = 0; i < v.size(); i++)
-	{
-		deque<int>queue;
-		for (int j = 0; j < 10; j++)
-		queue.push_back(rand() % 41 + 60);
-		sort(queue.begin(), queue.end());
-		queue.pop_back();
-		queue.pop_front();
-		int sum = 0;
-		for (int j = 0; j < queue.size(); j++)
-		sum += queue[i];
-		v[i].m_Score = sum / (int)queue.size();
-	}
-}
-
-void PlantPlayer(vector<Player>& v)
-{
-	for (int i = 0; i < v.size(); i++)
-		cout << "name:  " << v[i].m_Name << "\tScore:  " << v[i].m_Score << endl;
-}
-
 
 void test1()
 {
-	vector<Player>v;
-	creatPlayer(v);
-
-	setScore(v);
-
-	PlantPlayer(v);
-}
-
-void List()
-{
-	list<int>L1;
-	for (int i = 0; i < 10; i++)
-		L1.push_back(10 * i);
-	L1.insert(++L1.begin(),1000);
-	list<int>L2(L1.begin(),L1.end() );
-}
-
-//class Person
-//{
-//public:
-//	string m_Name;
-//	int m_Age;
-//};
-//
-//void test2()
-//{
-//	set<Person>s;
-//
-//}
-
-void test3()
-{
-	map<int, int>m;
-	m.insert(pair<int, int>(1, 10));
-	m.insert(pair<int, int>(5, 50));
-	m.insert(pair<int, int>(3, 30));
-	m.insert(pair<int, int>(4, 40));
-	for (int i = 1; i <= m.size(); ++i)
-		cout << m[i] << "  ";
-	for (map<int,int>::iterator it = m.begin(); it != m.end(); it++)
-	{
-		cout << it->first << "  " << it->second << "  " << endl;
-	}
-	map<int, int>m1(m);
-	for (map<int, int>::iterator it = m1.begin(); it != m1.end(); it++)
-	{
-		cout << it->first << "  " << it->second << "  " << endl;
-	}
-	map<int, int>m2 = m1;
-	for (map<int, int>::iterator it = m2.begin(); it != m2.end(); it++)
-	{
-		cout << it->first << "  " << it->second << "  " << endl;
-	}
-
-}
-
-class Emplayee
-{
-public:
-	Emplayee(string name, int salary)
-	{
-		this->m_Name = name;
-		this->m_Salary = salary;
-	}
-
-	
-
-	string m_Name;
-
-	int m_Salary;
-
-	string m_Branch;
-
-	int m_Size;
-};
-
-void operator<<(ostream& cout,Emplayee& p)
-{
-	cout << "姓名: " << p.m_Name << "\t所属部门: " << p.m_Branch << "\t薪资: " << p.m_Salary << endl;;
-}
-
-void CreatEmplayee(vector<Emplayee>& v)
-{
+	vector<int>v;
 	for (int i = 0; i < 10; ++i)
-	{
-		string s = "ABCDEFGHIJ";
-		string name = "员工";
-		name += s[i];
-		Emplayee p(name, 0);
-		v.push_back(p);
-	}
+		v.push_back(i);
+	if (v.end() != find_if(v.begin(), v.end(), five()))
+		cout << "Yes" << endl;
 }
-
-void DisEmp(vector<Emplayee>&v,multimap<int,Emplayee>&m)
-{
-	for (int i = 0; i < v.size(); ++i)
-	{
-		v[i].m_Size = rand() % 3 + 1;
-		switch (v[i].m_Size)
-		{
-		case 2:
-			v[i].m_Branch = "研发";
-			v[i].m_Salary = rand() % 5000 + 8000;
-			break;
-		case 3:
-			v[i].m_Branch = "策划";
-			v[i].m_Salary = rand() % 5000 + 5000;
-			break;
-		case 1:
-			v[i].m_Branch = "美术";
-			v[i].m_Salary = rand() % 5000 + 11000;
-			break;
-		default:
-			break;
-		}
-		m.insert(pair<int,Emplayee>(v[i].m_Size,v[i]));
-	}
-}
-
-void PrintEmp(multimap<int, Emplayee>& m)
-{
-	for (multimap<int, Emplayee>::iterator it = m.begin(); it != m.end(); ++it)
-	{
-		cout << it->second;
-	}
-}
-
-void test4()
-{
-	vector<Emplayee>v;
-	multimap<int, Emplayee>m;
-	CreatEmplayee(v);
-	DisEmp(v, m);
-	PrintEmp(m);
-	//v[0].m_Size = 1;
-	//v[0].m_Salary = 3000;
-	//v[0].m_Branch = "美术";
-	//cout << v[0];
-}
-
-void Build_Tree(int arr[], int tree[], int end, int start, int node)
-{
-	if (end == start)
-	tree[node] = arr[end];
-	else {
-		int mid = (start + end) / 2;
-		int node_left  = node * 2 + 1;
-		int node_right = node * 2 + 2;
-
-		Build_Tree(arr, tree, mid, start, node_left);
-		Build_Tree(arr, tree, end, mid + 1, node_right);
-		tree[node] = tree[node_left] + tree[node_right];
-	}
-}
-
-void updata_tree(int arr[], int tree[], int end, int start, int node, int idx, int val) {
-	if (end == start && end == idx)
-	{
-		arr[idx] = val;
-		tree[node] = arr[idx];
-	}
-	else {
-		int mid = (start + end) / 2;
-		if (idx > mid)
-		updata_tree(arr, tree, end, mid + 1, 2 * node + 2, idx, val);
-		else 
-		updata_tree(arr, tree, mid,start, 2 * node + 1, idx, val);
-		tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
-	}
-}
-
 void test()
 {
-	int arr[] = { 1,3,5,7,9,11 };
-	int size = 6;
-	int tree[15] = { 0 };
-
-	Build_Tree(arr, tree, size - 1, 0, 0);
-
-	updata_tree(arr, tree, size - 1, 0, 0, 4, 6);
-	for (int i = 0; i < 15; i++)
+	vector<int>v1;
+	vector<int>v2;
+	for (int i = 0; i < 5; ++i)
 	{
-		cout << tree[i] << " ";
+		v1.push_back(i);
+		v2.push_back(2 + i);
 	}
+	merge(v1.begin(), v1.end(), v2.begin(), v2.begin() + 5, v2.begin());
+	for (int i = 0; i < v2.size(); ++i)
+	cout << v2[i] << "  ";
+	cout << endl;
 }
 
 int main()
@@ -680,3 +800,4 @@ int main()
 	test();
 	return 0;
 }
+
